521542e67b0a7b6547ca6a1431e87ec4fbc4cde1
Abhishek Bhatia <abhibhatia@gmail.com>
2020-12-10 16:27:39 -0700

    checkin latest changes

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=abhibhatia/demolocal
 # Step 2:  
 # Authenticate & tag
+docker login --username  abhibhatia
+
+docker tag demolocal $dockerpath
+
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
